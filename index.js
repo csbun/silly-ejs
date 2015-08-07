fc8f9ecce250b674f45af1cd1aef0a44447e2ad2
Hans Chan <chenhc@ucweb.com>
2015-08-07 17:15:46 +0800

    减少 replace 次数

--- index.js
+++ index.js
@@ -21,23 +21,21 @@
 }(this, function() {
     'use strict';
 
-    // 代码部分不在字符串内，不需要 `\"` 和 `\\`
-    function codeStringBack (code) {
-        return code.replace(/\\"/g, '"').replace(/\\\\/g, '\\');
-    }
-
-    module.exports = function(str, data) {
+    return function(str, data) {
         var func = new Function('_data',
             'var __tpl="";with(_data){__tpl+="' + // 用 `""` 包裹字符串
             str.replace(/\\/g, '\\\\') // 字符串中的 `\` 要改成 `\\`
                 .replace(/"/g, '\\"') // 字符串中的 `"` 要改成 `\"`
-                .replace(/<%=([\s\S]+?)%>/g, function(match, code) {
-                    return '"+' + codeStringBack(code) + '+"';
-                })
-                .replace(/<%([\s\S]+?)%>/g, function(match, code) {
-                    return '";' + codeStringBack(code) + '__tpl+="';
-                })
-                .replace(/[\r\n\t]/g, ' ') +
+                .replace(/[\r\n\t]/g, ' ') // 去除字符串中的换行 TODO: 可能会去除掉代码内的这些内容，同时 <code> 内的也会被去到
+                .replace(/<%(=?)([\s\S]+?)%>/g, function(match, display, code) {
+                    // 代码部分不在字符串内，不需要 `\"` 和 `\\`
+                    var translatedCode = code.replace(/\\"/g, '"').replace(/\\\\/g, '\\');
+                    if (display) {
+                        return '"+' + translatedCode + '+"';
+                    } else {
+                        return '";' + translatedCode + '__tpl+="';
+                    }
+                }) +
             '"};return __tpl;');
         return func ? func(data).replace(/\s+/g, ' ') : '';
     };
--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@
 var assert = require('assert');
 var ejs = require('../index');
 
+console.log(ejs);
+
 var TPL = '<p><%= name %>\n' +
     '<% if (gender == "m")\n{ %>\n' +
     '\t<span class="g">male</span>\n'+
